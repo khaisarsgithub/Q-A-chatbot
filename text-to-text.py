c0031b44fd263c262edc4139b58ecae500d6723a
khaisarsgithub <mdkhaisarsmail118@gmail.com>
2024-01-23 12:57:34 +0530

    handle exception

--- text-to-text.py
+++ text-to-text.py
@@ -34,20 +34,23 @@ if selected_option == "Translate":
 
     if submit:
         st.write(text)
-
         # Create the prompt for translation
         prompt = f"Please translate '{text}' to '{target_language}.'"
         st.write("Translating...")
         # Generate text with the model
-        response = model.generate_content(prompt)
-        st.subheader("Transleted to " + target_language)
+        try:
+            response = model.generate_content(prompt)
+            st.subheader("Transleted to " + target_language)
 
-        # Extract the translated text from the response
-        translated_text = response.text
+            # Extract the translated text from the response
+            translated_text = response.text
 
-        # Print the translated text
-        print(f"Translated text: {translated_text}")
-        st.write(translated_text)
+            # Print the translated text
+            print(f"Translated text: {translated_text}")
+            st.write(translated_text)
+        except Exception as e:
+            print(e)
+            st.write("We are not in your language yet...")
         
 
     
@@ -58,11 +61,17 @@ elif selected_option == "Text Summary":
 
     if submit:
         st.text("Summarizing...")
-        prompt = f"Please Summarize ```{text}```  and give a short summary'"
-        response = model.generate_content(prompt)
-        print(response.text)
-        st.subheader("Summary:")
-        st.write(response.text)
+        prompt = f"Please Summarize the text delimeted by ```  text: ```{text}```  and give a short summary'"
+        try:
+            response = model.generate_content(prompt)
+            print(response.text)
+            st.subheader("Summary:")
+            st.write(response.text)
+        except Exception as e:
+            print(e)
+            st.write("Some Error Occured...")
+
+
 elif selected_option == "Data Analysis":
     st.header("Research and QA")
     url_1 = st.text_input("Enter URL1")
@@ -77,10 +86,14 @@ elif selected_option == "Data Analysis":
         data = loader.load()
         # print("Data: " + data)
         st.write("Summarizing the content...")
-        prompt = f"Please Summarize ```{data}```  and give a short summary'"
-        response = model.generate_content(prompt)
-        print(response.text)
-        st.write(response.text)
+        try:
+            prompt = f"Please Summarize the text delimited by ```  text: ```{data}```  and a short summary as well as the give key insights about the text'"
+            response = model.generate_content(prompt)
+            print(response.text)
+            st.write(response.text)
+        except Exception as e:
+            print(e)
+            st.write("Something went wrong ...")
         
     
 else:
@@ -93,8 +106,12 @@ else:
         data = loader.load()
         st.write("Generating...")
         data = data[0].page_content
-        prompt = f"List out the questions from the data delimeted by ``` data: ```{data}```'"
-        response = model.generate_content(prompt)
-        st.subheader("Generated Questions: ")
-        print(response.text)
-        st.write(response.text)
+        try:
+            prompt = f"List out the questions from the data delimeted by ``` data: ```{data}```'"
+            response = model.generate_content(prompt)
+            st.subheader("Generated Questions: ")
+            print(response.text)
+            st.write(response.text)
+        except Exception as e:
+            print(e)
+            st.write("Something went wrong ...")
